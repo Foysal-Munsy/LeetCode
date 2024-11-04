68e8a72c00982193623b96b02d71e6302177a9bd
Foysal Munsy <77909562+Foysal-Munsy@users.noreply.github.com>
2024-11-04 22:25:36 +0600

    Time: 52 ms (55.06%), Space: 27.3 MB (97.14%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,38 +1,42 @@
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        
-        set<list<int>>st;
-        for(int i=0; i<n-2; i++)
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> result;
+        for (int i = 0; i < n - 2; i++)
         {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int left = i+1 , right = n-1;
-            while(left<right)
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int left = i + 1, right = n - 1;
+            while (left < right)
             {
                 int sum = nums[i] + nums[left] + nums[right];
-                
-                if(sum == 0)
+
+                if (sum == 0)
                 {
-                    st.insert({nums[i],nums[left],nums[right]});
-                    left ++, right--;
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+                    
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+                    left++, right--;
                 }
-                else if(sum > 0)
+                else if (sum > 0)
                 {
                     right--;
                 }
-                else 
+                else
                 {
                     left++;
                 }
             }
         }
-        vector<vector<int>> result;
-        for (const auto& triplet : st) {
-            result.push_back(vector<int>(triplet.begin(), triplet.end()));
-        }
+        
         return result;
     }
-    
 };
